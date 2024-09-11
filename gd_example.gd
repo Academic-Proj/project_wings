81541dc6dad6ef1461f2b69c50d5add31049103c
Kaizu-r <jljoloc@gmail.com>
2024-09-12 00:15:30 +0800

    initial commit

---
+++ gd_example.gd
@@ -0,0 +1,31 @@
+extends GDExample
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_north_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_south_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_east_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_west_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
